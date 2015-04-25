0c23d19d91c6472ca43774cd7b4206853ad2d1eb
AllieT <madebyallie@users.noreply.github.com>
2015-04-25 14:33:32 +0100

    Initial Map files
    map.php map.json and javascript files app_mapp.js and app_map.js created
    at the beginning of the project

---
+++ js/app_map.js
@@ -0,0 +1,71 @@
+var map;
+
+function getPubs (callback) {
+	$.getJSON('js/map.json', callback)
+}
+
+function initialize() {
+  var mapOptions = {
+    zoom: 15
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+  
+  getPubs(function (data) {
+		var hostelries = data.hostelries;
+		var hostelry, latLng;
+
+		for (i in hostelries) {
+			hostelry = hostelries[i];
+			latLng = new google.maps.LatLng(hostelry.latitude, hostelry.longitude)
+
+			var marker = new google.maps.Marker({
+				position: latLng,
+				map: map,
+				title: hostelry.name
+			})
+		}
+	})
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ map.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+		<meta charset="utf-8">
+		<title>London Pub Crawl</title>
+		<link href="https://developers.google.com/maps/documentation/javascript/examples/default.css" rel="stylesheet">
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+
+		<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+		<script src="js/app_mapp.js"></script>
+	</head>
+	<body>
+		<div id="map-canvas"></div>
+	</body>
+</html>
