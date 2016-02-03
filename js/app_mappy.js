76b72e3b019d07a28a1d39b62af462854e2ce76a
AllieT <madebyallie@users.noreply.github.com>
2016-02-03 18:04:56 +0000

    New Maps for 2016
    Think of it as a new beginning.

--- js/app_mappy.js
+++ js/app_mappy.js
@@ -22,7 +22,7 @@ function initialize() {
 
   map.setCenter(options.position);
 
-  var infowindow = new google.maps.InfoWindow();
+  //var infowindow = new google.maps.InfoWindow();
 
   getPubs(function (data) {
     var hostelries = data.hostelries;
@@ -39,13 +39,18 @@ function initialize() {
       review: hostelry.review
 		});
 
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(this.title + '<br />' + this.review);
-        infowindow.open(map, this);
+    //google.maps.event.addListener(marker, 'click', function () {
+        //infowindow.setContent(this.title + '<br />' + this.review);
+        //infowindow.open(map, this);
+    //});
+        google.maps.event.addListener(marker, 'click', function () {
+        document.getElementById("pub-name").innerHTML = (this.title),
+        document.getElementById("pub-review").innerHTML = (this.review);
     });     
   }
   });
 }
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
--- map.php
+++ map.php
@@ -3,11 +3,29 @@
 	<head>
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 		<meta charset="utf-8">
-		<title>London Pub Crawl</title>
-		<link href="https://developers.google.com/maps/documentation/javascript/examples/default.css" rel="stylesheet">
+		<title>A London Pub Crawl Google Map | madebyallie | Chichester Design</title>
+		<script src="bower_components/modernizr/modernizr.js"></script>
+	    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="../stylesheets/app.css" />
+		<link rel="stylesheet" href="stylesheets/map.css" />
 	</head>
 	<body>
-		<div id="map-canvas"></div>
+		<div class="medium-3 columns mapintro">
+			<h1 class="text-center"><span>London Pubs</span></h1>
+			<p class="text-center">it doesn't have to be a crawl...</p>
+			<hr/>
+			<div id="pub-details">
+				<h4>Pub Name: </h4>
+				<div id="pub-name">Click on a map marker for pub details</div>
+				<h4>Pub Review: </h4>
+				<div id="pub-review"></div>
+			</div>
+		</div>
+		<div class="medium-9 columns no-padding">
+			<div class="mapholder">
+				<div id="map-canvas" class="map-canvas"></div>
+			</div>
+		</div>
 		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
 		<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 		<script src="js/app_mappy.js"></script>
